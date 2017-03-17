4c4c9c1a9dbaf60046b97a4baea7342d23163300
Bruce Szalwinski <bruce.szalwinski@cdk.com>
2017-03-17 14:20:44 -0500

    convert to maven build

--- ConsumerExample/src/main/java/com/kafka/tutorial/NewSupplierConsumer.java
+++ ConsumerExample/src/main/java/com/kafka/tutorial/NewSupplierConsumer.java
@@ -1,26 +1,29 @@
+package com.kafka.tutorial;
+
 import java.util.*;
 import java.io.*;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class ManualConsumer{
+public class NewSupplierConsumer{
 
     public static void main(String[] args) throws Exception{
 
         String topicName = "SupplierTopic";
         String groupName = "SupplierTopicGroup";
-
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-        props.put("group.id", groupName);
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "SupplierDeserializer");
-        props.put("enable.auto.commit", "false");
+        //props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+        //props.put("group.id", groupName);
+        //props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        //props.put("value.deserializer", "SupplierDeserializer");
 
+        InputStream input = null;
         KafkaConsumer<String, Supplier> consumer = null;
 
         try {
+            input = new FileInputStream("SupplierConsumer.properties");
+            props.load(input);
             consumer = new KafkaConsumer<>(props);
             consumer.subscribe(Arrays.asList(topicName));
 
@@ -29,12 +32,11 @@ public class ManualConsumer{
                 for (ConsumerRecord<String, Supplier> record : records){
                     System.out.println("Supplier id= " + String.valueOf(record.value().getID()) + " Supplier  Name = " + record.value().getName() + " Supplier Start Date = " + record.value().getStartDate().toString());
                 }
-                consumer.commitAsync();
             }
         }catch(Exception ex){
             ex.printStackTrace();
         }finally{
-            consumer.commitSync();
+            input.close();
             consumer.close();
         }
     }
